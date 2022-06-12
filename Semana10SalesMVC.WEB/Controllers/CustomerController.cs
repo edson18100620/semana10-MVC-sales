02b25f86118928ee32735c53a0494085ce9d1594
Luis Chang <luis.chang@outlook.com>
2022-06-12 13:18:16 -0500

    Create customer in modal

--- Semana10SalesMVC.WEB/Controllers/CustomerController.cs
+++ Semana10SalesMVC.WEB/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Semana10SalesMVC.WEB.Models;
 using Semana10SalesMVC.WEB.Service;
 
 namespace Semana10SalesMVC.WEB.Controllers
@@ -15,6 +16,31 @@ namespace Semana10SalesMVC.WEB.Controllers
             var customers = await CustomerService.GetAll();
             return PartialView(customers);        
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Guardar(int idCliente, string nombre,
+            string apellido, string ciudad, string pais, string telefono)
+        {
+            var customer = new CustomerModel()
+            {
+                FirstName = nombre,
+                LastName = apellido,
+                City = ciudad,
+                Country = pais,
+                Phone = telefono
+            };
+
+            bool exito = true;
+            if (idCliente == -1)
+                exito = await CustomerService.Insert(customer);
+            else {
+                customer.Id = idCliente;
+                exito = await CustomerService.Update(idCliente, customer);
+            }
+            return Json(exito);
+        }
+
+
         
     }
 }
