5ef3a8adb3ab3bdfd4822a5e3c065e494bccfdbd
Luis Chang <luis.chang@outlook.com>
2022-06-12 13:03:01 -0500

    CRUD CustomerService

--- Semana10SalesMVC.WEB/Service/CustomerService.cs
+++ Semana10SalesMVC.WEB/Service/CustomerService.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Semana10SalesMVC.WEB.Models;
+using System.Text;
 
 namespace Semana10SalesMVC.WEB.Service
 {
@@ -16,7 +17,43 @@ namespace Semana10SalesMVC.WEB.Service
             string apiResponse = response.Content.ReadAsStringAsync().Result;
             var customers = JsonConvert.DeserializeObject<IEnumerable<CustomerModel>>(apiResponse);
             return customers;
-        }        
-        
+        }
+
+        //Create new Customer
+        public static async Task<bool> Insert(CustomerModel customer)
+        {
+            string urlBase = "http://localhost:5047/api/Customer/";
+
+            using var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(customer);
+            var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+            using var response = await httpClient.PostAsync(urlBase + "Insert", stringContent);
+            string apiResponse = response.Content.ReadAsStringAsync().Result;
+            var result = JsonConvert.DeserializeObject<bool>(apiResponse);
+            return result;
+        }
+
+        //Update Customer
+        public static async Task<bool> Update(int id, CustomerModel customer)
+        {
+            string urlBase = "http://localhost:5047/api/Customer/";
+            using var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(customer);
+            var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+            using var response = await httpClient.PutAsync(urlBase + "Update/" + id, stringContent);
+            string apiResponse = response.Content.ReadAsStringAsync().Result;
+            var result = JsonConvert.DeserializeObject<bool>(apiResponse);
+            return result;
+        }
+        //Delete Customer
+        public static async Task<bool> Delete(int id)
+        {
+            string urlBase = "http://localhost:5047/api/Customer/";
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.DeleteAsync(urlBase + "Delete/" + id);
+            string apiResponse = response.Content.ReadAsStringAsync().Result;
+            var result = JsonConvert.DeserializeObject<bool>(apiResponse);
+            return result;
+        }
     }
 }
